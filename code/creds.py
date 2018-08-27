31264ea05a2497f7572d12ce953ba9c1e52e8aec
joe brower <zyoosun@gmail.com>
2018-08-27 11:14:14 -0500

    removing creds

--- code/creds.py
+++ code/creds.py
@@ -1,5 +1,5 @@
 
-headers = {"Authorization": "Bearer 40aa490a8c311cf5ddebfe9d8dd7648842b7983f438b6913194803c7a00761a8", "Content-type": "application/json"}
+headers = {"Authorization": "Bearer ", "Content-type": "application/json"}
 
 particulars = {
         "per_page": 100
